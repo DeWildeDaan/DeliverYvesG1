fe6970854db18bb70b8e885dddcbd85dfc77f96b
alec gheeraert <gheeraert.alec@gmail.com>
2022-05-20 12:27:38 +0100

    startup, ultrasonic

--- industry/industry.ino
+++ industry/industry.ino
@@ -1,7 +1,42 @@
-void setup() {
+// Libraries
+#include <HX711.h>
 
+// Ultrasonic High
+#define pin_h_trigger 12
+#define pin_h_echo 13
+
+// Ultrasonic Low
+#define pin_l_trigger 27
+#define pin_l_echo 26
+
+// Weight sensors
+#define pin_data 33
+#define pin_clk 32
+HX711 scale;
+
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(pin_h_trigger, OUTPUT);
+  pinMode(pin_l_trigger, OUTPUT);
+  pinMode(pin_h_echo, INPUT);
+  pinMode(pin_l_echo, INPUT);
 }
 
 void loop() {
+  int distance_h = ultrasonicDistances(pin_h_trigger, pin_h_echo);
+  Serial.println(distance_h);
+}
 
+int ultrasonicDistanceH(int pin_trigger, int pin_echo){
+  digitalWrite(pin_trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pin_trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pin_trigger, LOW);
+
+  int duration = pulseIn(pin_echo, HIGH);
+  int distance = duration * 0.017;
+  return distance;
 }
