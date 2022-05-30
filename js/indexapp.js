c02bb10406e3c2759ec801a3cf20cbb5798262f9
Daan De Wilde <daan.de.wilde@student.howest.be>
2022-05-30 09:40:56 +0200

    index js

---
+++ js/indexapp.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//#region ***  DOM references                           ***********
+//#endregion
+
+//#region ***  Callback-Visualisation - show___         ***********
+//#endregion
+
+//#region ***  Callback-No Visualisation - callback___  ***********
+//#endregion
+
+//#region ***  Data Access - get___                     ***********
+//#endregion
+
+//#region ***  Event Listeners - listenTo___            ***********
+// Event listeners
+
+//#region ***  Init / DOMContentLoaded                  ***********
+const init = function () {
+	console.log('DOM Content Loaded.');
+};
+
+document.addEventListener('DOMContentLoaded', init);
+//#endregion
