008094345c07fd3c61ab3397db3d33fa88f80fb3
Daan De Wilde <daan.de.wilde@student.howest.be>
2022-05-31 15:27:29 +0200

    js

--- js/indexapp.js
+++ js/indexapp.js
@@ -1,21 +1,48 @@
 "use strict";
 let baseUrl =
   "https://deliveryevesg1minimalapi.livelygrass-d3385627.northeurope.azurecontainerapps.io";
+let customerList = [];
+let customers;
+let previousCustomer;
 
 //#region ***  DOM references                           ***********
-let customerList;
+let htmlCustomerList, htmlSortButton, htmlCustomerSearch;
+
 //#endregion
 
 //#region ***  Callback-Visualisation - show___         ***********
 const showCustomers = function () {
-  let html = customerList.innerHTML;
+  let html = ``;
   html += `
-                        <button class="o-button-reset c-customer-button">
-                            <p class="o-remove-margin">${name}</p>
-                            <p class="o-remove-margin">${total}</p>
+                        <button class="o-button-reset c-customer-button c-customer-button_selected js-customer-button" data-customerId=${customerList[0].customerId}>
+                            <p class="o-remove-margin">${customerList[0].name}</p>
+                            <p class="o-remove-margin">${customerList[0].total}</p>
                         </button>
   `;
-  customerList.innerHTML = html;
+  for (let customer of customerList) {
+    html += `
+                        <button class="o-button-reset c-customer-button js-customer-button" data-customerId=${customer.customerId}>
+                            <p class="o-remove-margin">${customer.name}</p>
+                            <p class="o-remove-margin">${customer.total}</p>
+                        </button>
+  `;
+  }
+  htmlCustomerList.innerHTML = html;
+  listenToCustomerButton();
+};
+
+const showFilteredCustomers = function (arr) {
+  let html = ``;
+  for (let customer of arr) {
+    html += `
+                        <button class="o-button-reset c-customer-button js-customer-button" data-customerId=${customer.customerId}>
+                            <p class="o-remove-margin">${customer.name}</p>
+                            <p class="o-remove-margin">${customer.total}</p>
+                        </button>
+  `;
+  }
+  htmlCustomerList.innerHTML = html;
+  listenToCustomerButton();
 };
 //#endregion
 
@@ -26,41 +53,90 @@ const callbackError = function (jsonObject) {
 
 const callbackCustomers = function (jsonObject) {
   for (let customer of jsonObject) {
-    customer["TotalPredictions"] = getPredictionsCustomer(customer.Id);
+    callbackGetTotalPredictions(customer.Id, customer.Name);
   }
 };
 
-const callbackTotalPredictions = function (jsonObject) {
-  console.log(jsonObject);
+const callbackGetTotalPredictions = function (customerId, name) {
+  let url = `${baseUrl}/totalpredictions`;
+  const body = JSON.stringify({
+    CustomerId: customerId,
+    Name: name,
+  });
+  handleData(url, callbackTotalPredictions, callbackError, "POST", body);
 };
+
+const callbackTotalPredictions = function (jsonObject) {
+  customerList.push(jsonObject);
+  if (customerList.length == customers.length) {
+    customerList.sort(function (first, second) {
+      return second.total - first.total;
+    });
+    showCustomers();
+  }
+};
+
+const callbackSortCustomers = function () {};
 
 //#endregion
 
 //#region ***  Data Access - get___                     ***********
 const getCustomers = function () {
   //In realiteit spreek je de klantendatabase aan
-  let customers = [
-    { Name: "Daan", Id: "1" },
-    { Name: "Alec", Id: "2" },
+  customers = [
+    { Name: "daan", Id: "1" },
+    { Name: "alec", Id: "2" },
   ];
   callbackCustomers(customers);
-};
-
-const getPredictionsCustomer = function (customerId) {
-  let url = `${baseUrl}/totalpredictions/${customerId}`;
-  handleData(url, callbackTotalPredictions, callbackError);
 };
 //#endregion
 
 //#region ***  Event Listeners - listenTo___            ***********
+const listenToSortButton = function () {
+  htmlSortButton.addEventListener("click", function () {
+    htmlSortButton.classList.toggle("c-title-left_arrow-flip");
+    customerList.reverse();
+    htmlCustomerSearch.value = "";
+    showCustomers();
+  });
+};
+
+const listenToCustomerSearch = function () {
+  htmlCustomerSearch.addEventListener("input", function () {
+    let arr = customerList.filter(function (elem) {
+      return (
+        elem.name.includes(htmlCustomerSearch.value.toLowerCase()) ||
+        elem.customerId.includes(htmlCustomerSearch.value.toLowerCase())
+      );
+    });
+    showFilteredCustomers(arr);
+  });
+};
+
+const listenToCustomerButton = function () {
+  for (const b of document.querySelectorAll(".js-customer-button")) {
+    b.addEventListener("click", function () {
+      previousCustomer.classList.remove("c-customer-button_selected");
+      this.classList.add("c-customer-button_selected");
+      let customerId = this.getAttribute("data-customerId");
+      previousCustomer = this;
+      console.log(customerId);
+    });
+  }
+};
 // Event listeners
 
 //#region ***  Init / DOMContentLoaded                  ***********
 const init = function () {
   console.log("DOM Content Loaded.");
-  customerList = document.querySelector(".js-customer-list");
+  htmlCustomerList = document.querySelector(".js-customer-list");
+  htmlSortButton = document.querySelector(".js-sort-button");
+  htmlCustomerSearch = document.querySelector(".js-search");
 
   getCustomers();
+
+  listenToSortButton();
+  listenToCustomerSearch();
 };
 
 document.addEventListener("DOMContentLoaded", init);
